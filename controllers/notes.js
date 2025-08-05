1c906d9ebec845c5a9d3fa033a0aa2280e8e34ca
Jay Li <89029831+jay-li123@users.noreply.github.com>
2025-08-05 18:58:35 -0400

    part4-9 --3

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,11 +1,11 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note
-    .find({}).populate('user', { username: 1, name: 1 })
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
+
   response.json(notes)
 })
 
@@ -20,7 +20,7 @@ notesRouter.get('/:id', async (request, response) => {
 
 const getTokenFrom = request => {
   const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer')) {
+  if (authorization && authorization.startsWith('Bearer ')) {
     return authorization.replace('Bearer ', '')
   }
   return null
@@ -28,16 +28,15 @@ const getTokenFrom = request => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+
   const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
   if (!decodedToken.id) {
-    return response.status(401).json({
-      error: 'token invalid '
-    })
+    return response.status(401).json({ error: 'token invalid' })
   }
   const user = await User.findById(decodedToken.id)
 
   if (!user) {
-    return response.status(400).json({ error: 'userId missing or not valid' })
+    return response.status(400).json({ error: 'UserId missing or not valid' })
   }
 
   const note = new Note({
@@ -62,7 +61,7 @@ notesRouter.put('/:id', (request, response, next) => {
   const { content, important } = request.body
 
   Note.findById(request.params.id)
-    .then((note) => {
+    .then(note => {
       if (!note) {
         return response.status(404).end()
       }
@@ -70,11 +69,11 @@ notesRouter.put('/:id', (request, response, next) => {
       note.content = content
       note.important = important
 
-      return note.save().then((updatedNote) => {
+      return note.save().then(updatedNote => {
         response.json(updatedNote)
       })
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
 module.exports = notesRouter
